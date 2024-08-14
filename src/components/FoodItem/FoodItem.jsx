333228b579a4053598c981417e55c8c110a86d7d
dandugulaVenky <venkatdandugulayou@gmail.com>
2024-08-14 22:57:01 +0530

    first commit

---
+++ src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,11 @@
+import './FoodItem.css'
+
+const FoodItem = ({id,name,price,discription,image}) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default FoodItem
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='hader-contents'>
+           <h2>Order yours favourite food here </h2>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi architecto optio, provident facere temporibus vel possimus voluptatem debitis. Iste at fugit numquam, pariatur, sed dolor consectetur, culpa quibusdam excepturi aliquam dolore aspernatur. Velit, ad.</p>
+           <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
