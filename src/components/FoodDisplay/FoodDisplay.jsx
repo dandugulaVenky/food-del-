9abdcf1620456a47cd001c57c36bbe1156831248
dandugulaVenky <venkatdandugulayou@gmail.com>
2024-08-15 15:36:42 +0530

    images updated

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,24 +1,25 @@
-import { useContext } from 'react'
-import './fooddisplay.css'
-import { StoreContext } from './../../context/StoreContext';
-import FoodItem from '../FoodItem/FoodItem';
+import { useContext } from "react";
+import "./fooddisplay.css";
+import { StoreContext } from "./../../context/StoreContext";
+import FoodItem from './../FoodItem/FoodItem';
 
-const FoodDisplay = ({category}) => {
 
-const food_list = useContext(StoreContext)
+
+
+const FoodDisplay = ({ category }) => {
+  const {food_list} = useContext(StoreContext);
 
   return (
-    <div className='food-display' id='food-display'>
-        <h2>Top dishes near you</h2>
-        <div className='food-display-list'>
-           {/* {food_list.map((item,index)=>{
-            return <FoodItem key={item} id={item.id} name={item.name} price={item.price} descrtption={item.descrtption} image={item.image} />
-           })} */}
+    <div className="food-display" id="food-display">
+      <h2>Top dishes near you</h2>
+      <div className="food-display-item">
+      {food_list.map((item, index) => {
+          return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+        })}
 
-        </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
