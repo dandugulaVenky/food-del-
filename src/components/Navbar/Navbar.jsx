2c664d129f9e6314efbc96787078fad09f81257b
dandugulaVenky <venkatdandugulayou@gmail.com>
2024-08-16 13:32:24 +0530

    addtocart page done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({setShowLogIn}) => {
 
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt='logo' className='logo' />
+        <Link to='/'> <img src={assets.logo} alt='logo' className='logo' /> </Link>
          <ul className="navbar-menu">
             <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
             <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</a>
@@ -21,7 +21,7 @@ const Navbar = ({setShowLogIn}) => {
          <div className="navbar-right">
             <img src={assets.search_icon} alt=''/>
            <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt=''/>
+          <Link to='/cart'><img src={assets.basket_icon} alt=''/></Link>
             <div className="dot"></div>
            </div>
            <button onClick={()=>setShowLogIn(true)} >sign in</button>
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,8 +1,49 @@
 import './Cart.css'
+import { food_list } from './../../assets/assets';
+import { useContext } from 'react';
+import { StoreContext } from './../../context/StoreContext';
 
 const Cart = () => {
+
+  const {cardItems,food_list,removeFromCart} = useContext(StoreContext);
+
+
+
+
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item,index)=>{
+            if(cardItems[item._id]>0)
+            {
+              return (
+               <div>
+                 <div className="cart-items-title cart-items-item">
+                <img src={item.image} alt="" />
+                <p>{item.name}</p>
+                <p>${item.price}</p>
+                <p>{cardItems[item._id]}</p>
+                <p>${item.price*cardItems[item._id]}</p>
+                <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
+                </div>
+                <hr />
+               </div>
+              )
+            }
+        })}
+      </div>
+
+    </div>
   )
 }
 
