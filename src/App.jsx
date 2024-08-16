3e102dd08fd77cc44a5d59cb8d3cba0526f84677
dandugulaVenky <venkatdandugulayou@gmail.com>
2024-08-16 12:29:01 +0530

    sign & login pages created

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,18 @@ import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Footer from "./components/Footer/Footer";
+import { useState } from "react";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
 
 const App = () => {
+
+   const [showLogin,setShowLogIn] = useState(false)
+
   return (
     <>
+    {showLogin?<LoginPopup setShowLogIn={setShowLogIn} />:<></>}
      <div  className="app">
-      <Navbar />
+      <Navbar setShowLogIn={setShowLogIn} />
      <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path='/cart' element={<Cart/>}></Route>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { assets } from './../../assets/assets';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({setShowLogIn}) => {
 
    const [menu, setMenu] =useState("home")
 
@@ -24,7 +24,7 @@ const Navbar = () => {
             <img src={assets.basket_icon} alt=''/>
             <div className="dot"></div>
            </div>
-           <button>sign in</button>
+           <button onClick={()=>setShowLogIn(true)} >sign in</button>
          </div>
 
     </div>
